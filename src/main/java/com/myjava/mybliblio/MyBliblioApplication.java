bffeb5dca28a89a26957e3fa63f7daed23fa8c0a
Ramm759 <fredgsn59@gmail.com>
2023-09-10 21:03:29 +0200

    Config Mysql

---
+++ src/main/java/com/myjava/mybliblio/MyBliblioApplication.java
@@ -0,0 +1,13 @@
+package com.myjava.mybliblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBliblioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBliblioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myjava/mybliblio/MyBliblioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjava.mybliblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBliblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
