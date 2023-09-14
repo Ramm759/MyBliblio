ea8c891dae37a4ace78e740b67ecbc67f37ce407
Ramm759 <fredgsn59@gmail.com>
2023-09-14 13:21:46 +0200

    Crud Rest + Swagger

---
+++ src/main/java/com/myjava/mybliblio/entity/Tutorial.java
@@ -0,0 +1,31 @@
+package com.myjava.mybliblio.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Tutorial(String title, String description, boolean published) {
+
+    }
+}
---
+++ src/main/java/com/myjava/mybliblio/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package com.myjava.mybliblio.repository;
+
+import com.myjava.mybliblio.entity.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
